e8d4e7e70888bf4e5f9c752b224fa0749016e651
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2024-01-23 15:26:03 -0500

    se agregaron otros cambios

--- main.c
+++ main.c
@@ -8,24 +8,33 @@ struct Alumno {
     float nota;
 };
 
+void mostrarDatos(struct Alumno alumno) {
+    printf("Matrícula: %d\n", alumno.matricula);
+    printf("Nombre: %s\n", alumno.nombre);
+    printf("Dirección: %s\n", alumno.direccion);
+    printf("Materia: %s\n", alumno.materia);
+    printf("Nota: %.2f\n\n", alumno.nota);
+}
+
 int main() {
     int numEstudiantes;
     struct Alumno alumno;
     FILE *archivo;
 
-    // se ingresan el número de cualquier estudiantes
+    // Ingresar el número de estudiantes
     printf("Ingrese el número de estudiantes: ");
     scanf("%d", &numEstudiantes);
 
-    // sirve para poder abrir el archivo y se guarden los datos dentro de un archivo llamado: 
+    // Abrir el archivo en modo escritura
     archivo = fopen("datos_alumnos.txt", "w");
 
-    // aqui para poder verificar si el archivo se abrio correctamente
+    // Verificar si el archivo se abrió correctamente
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo.\n");
         return 1;
     }
-    // se puede mostrar aqui el ingreso de de cada estudieante y guardarlos de forma horizontal
+
+    // Ingresar los datos de cada estudiante y guardarlos en el archivo
     for (int i = 0; i < numEstudiantes; i++) {
         printf("\nIngrese los datos del estudiante %d:\n", i + 1);
 
@@ -44,20 +53,23 @@ int main() {
         printf("Nota: ");
         scanf("%f", &alumno.nota);
 
-        // aqui escribira todos los datos y los pasara en un archivo y los mostrara de forma horizontal
+        // Escribir los datos del estudiante en el archivo
         fprintf(archivo, "Estudiante %d:\n", i + 1);
-        fprintf(archivo, "Matrícula: %d\t", alumno.matricula);
-        fprintf(archivo, "Nombre: %s\t", alumno.nombre);
-        fprintf(archivo, "Dirección: %s\t", alumno.direccion);
-        fprintf(archivo, "Materia: %s\t", alumno.materia);
-        fprintf(archivo, "Nota: %.2f\n\n", alumno.nota); //el %.2f es para poner la nota con decimales ejem: 10=10.00
+        fprintf(archivo, "Matrícula: %d\n", alumno.matricula);
+        fprintf(archivo, "Nombre: %s\n", alumno.nombre);
+        fprintf(archivo, "Dirección: %s\n", alumno.direccion);
+        fprintf(archivo, "Materia: %s\n", alumno.materia);
+        fprintf(archivo, "Nota: %.2f\n\n", alumno.nota);
+
+        // Mostrar los datos del estudiante en la consola
+        printf("\nDatos del estudiante %d:\n", i + 1);
+        mostrarDatos(alumno);
     }
-    // sirve poara poder cerrar el archivo
+
+    // Cerrar el archivo
     fclose(archivo);
 
-    // aqui es lo para mostrar que el archivo salio bien y se ha guardado en archivo txt
     printf("\nLos datos se han guardado en el archivo datos_alumnos.txt.\n");
 
-    
     return 0;
 }
